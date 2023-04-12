cc974a482a904f15879ac39123f501d054639b17
jennyyang <yzhenjuan@gmail>
2023-04-12 13:50:59 +1000

    Start Signin

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Header from './Components/Header_footer/header';
 import Footer from './Components/Header_footer/footer';
 import Home from './Components/Home';
+import SignIn from './Components/Signin';
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       {/* header */}
       <Header />
       <Switch>
+        <Route path='/sign_in' exact component={SignIn} />
         <Route path='/' exact component={Home} />
       </Switch>
       <Footer />
