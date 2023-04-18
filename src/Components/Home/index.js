82c93ef6a2e0da8b866182fde7c33f7962dbd774
jennyyang <yzhenjuan@gmail>
2023-04-18 15:06:29 +1000

    Finished Meet The Players

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './featured';
 import MatchesHome from './matches';
+import MeetPlayers from './meetplayers';
 
 const Home = () => {
   return (
     <div className='bck_blue'>
       <Featured></Featured>
       <MatchesHome></MatchesHome>
+      <MeetPlayers></MeetPlayers>
     </div>
   );
 };
