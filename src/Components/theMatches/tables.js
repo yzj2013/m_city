ab8c66f094eee4c89256f1ec23c36574ddf6dd40
jennyyang <yzhenjuan@gmail>
2023-04-27 11:44:19 +1000

    Finished Filter matches

---
+++ src/Components/theMatches/tables.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { positionsCollection } from '../../firebase';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+
+const LeagueTable = () => {
+  const [positions, setPositions] = useState(null);
+
+  useEffect(() => {
+    if (!positions) {
+      positionsCollection.get().then((snapshot) => {
+        const positions = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPositions(positions);
+      });
+    }
+  }, [positions]);
+
+  // console.log(position);
+
+  const showTeamPositions = () =>
+    positions
+      ? positions.map((pos, i) => (
+          <TableRow key={i}>
+            <TableCell>{i + 1}</TableCell>
+            <TableCell>{pos.team}</TableCell>
+            <TableCell>{pos.w}</TableCell>
+            <TableCell>{pos.d}</TableCell>
+            <TableCell>{pos.l}</TableCell>
+            <TableCell>{pos.pts}</TableCell>
+          </TableRow>
+        ))
+      : null;
+
+  return (
+    <div className='league_table_wrapper'>
+      <div className='title'>League Table</div>
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Pos</TableCell>
+              <TableCell>Team</TableCell>
+              <TableCell>W</TableCell>
+              <TableCell>L</TableCell>
+              <TableCell>D</TableCell>
+              <TableCell>Pts</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{showTeamPositions()}</TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default LeagueTable;
